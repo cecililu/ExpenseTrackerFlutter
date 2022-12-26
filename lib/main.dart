099881dda3535c9ea36cca7527913068bb647104
cecil <cecilghimire@gmail.com>
2022-12-26 18:21:07 +0545

    stying on the list

--- lib/main.dart
+++ lib/main.dart
@@ -41,26 +41,49 @@ class MyHomePage extends StatelessWidget {
               children: transactions.map((transaction) {
             return Card(
                 child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.center ,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
                 Container(
-                  color: Colors.green,
-      
-                  child: Text(transaction.amount.toString()),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.green,
+                      width: 2,
+                    ),
+                  ),
+                  margin: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                  padding: EdgeInsets.all(12),
+                  child: Text(
+                    transaction.amount.toString(),
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.green,
+                        fontSize: 25),
+                  ),
                 ),
                 Column(
-                  
+                  crossAxisAlignment:  CrossAxisAlignment.start,
                   children: <Widget>[
-                  Card(  
-                    color:Colors.red,
-                  child: Column(
-                    children: <Widget>[
-                      Text(transaction.title),
-                      Text(transaction.date.toString()),
-                    ],
-                  ),
-                  )
-                ],)
+                    Container(
+                      
+                      
+                      child: Column(
+                        children: <Widget>[
+                          Text(
+                            transaction.title,
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                                color: Colors.black),
+                          ),
+                          Text(transaction.date.toString(),
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w500,fontSize: 10,
+                                  color: Colors.grey)),
+                        ],
+                      ),
+                    )
+                  ],
+                )
               ],
             ));
           }).toList())
