519159e56dbb45e9cd02b165064c42885a8b3b62
cecil <cecilghimire@gmail.com>
2022-12-27 09:52:38 +0545

    gesture Detector to stop default behaviour by empy on Tap named parameter

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/models/transaction.dart';
 import 'package:flutter_complete_guide/widgets/new_transaction.dart';
 import 'package:flutter_complete_guide/widgets/transactionList.dart';
-import 'package:flutter_complete_guide/widgets/user_transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -15,44 +15,78 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  final List<Transaction> _userTransactions = [
+    Transaction(
+        id: 't1', title: 'New Jordans 4s', amount: 89.33, date: DateTime.now()),
+    Transaction(
+        id: 't2', title: 'New Jordans 2', amount: 89.33, date: DateTime.now()),
+  ];
+
+  void _addNewTransaction(String titletx, double amounttx) {
+    final newTrans = Transaction(
+        title: titletx,
+        amount: amounttx,
+        id: DateTime.now().toString(),
+        date: DateTime.now());
+    setState(() {
+      _userTransactions.add(newTrans);
+    });
+  }
+
+  void _startAddNewTranasction(ctx) {
+    showModalBottomSheet(
+        context: ctx,
+        builder: (bctx) {
+          return GestureDetector(
+              onTap: () {}, child: NewTransactionForm(_addNewTransaction));
+        });
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          actions: <Widget>[
-            IconButton(onPressed: () {}, icon: Icon(Icons.add))
-          ],
-          backgroundColor: Colors.deepOrange,
-          title: Text(
-            'Expense Tracker',
-            textAlign: TextAlign.center,
-          ),
+      appBar: AppBar(
+        actions: <Widget>[
+          IconButton(
+              onPressed: () => _startAddNewTranasction(context),
+              icon: Icon(Icons.add))
+        ],
+        backgroundColor: Colors.deepOrange,
+        title: Text(
+          'Expense Tracker',
+          textAlign: TextAlign.center,
         ),
-        body: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                width: double.infinity,
-                padding: EdgeInsets.all(20),
-                color: Colors.deepOrangeAccent,
-                child: Card(
-                  child: Text(
-                    "Chart Position",
-                    textAlign: TextAlign.center,
-                  ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              padding: EdgeInsets.all(20),
+              color: Colors.deepOrangeAccent,
+              child: Card(
+                child: Text(
+                  "Chart Position",
+                  textAlign: TextAlign.center,
                 ),
               ),
-              UserTransaction()
-            ],
-          ),
+            ),
+            TransactionList(_userTransactions)
+          ],
         ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {  },
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+          onPressed: () => _startAddNewTranasction(context),
           child: Icon(Icons.add)),
-        );
+    );
   }
 }
